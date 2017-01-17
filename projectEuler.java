93dfff5f576f97ae285a67b48ca1d2b38a3ec79e
jpreich <jpreich@live.com>
2017-01-16 14:34:12 -1000

    Update projectEuler.java

--- projectEuler.java
+++ projectEuler.java
@@ -1,24 +1,7 @@
-public class whatTheClass {
+public class projectEuler {
 	public static void main(String[] args){
 		
 	}
 	
-	public static int problemSix(){
-		int endNumber = 100;
-		int sumSquare = 0;
-		int squareSum = 0;
-		int tempSum = 0;
-		int difference;
-		for(int i = 1; i <= endNumber; i ++){
-			tempSum += i;
-			sumSquare += i * i;
-		}
-		squareSum = tempSum * tempSum;
-		difference = Math.abs(sumSquare - squareSum);
-		System.out.println(difference);
-		return difference;
-	}
-	public static void problemSeven(){
-		
-	}
+
 }
